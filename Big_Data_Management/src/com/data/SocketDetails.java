609a62980e245fd6f2d974d9b280e4d8b457fcb4
Lefteris Orfanidis <leforfanidis@gmail.com>
2021-04-04 19:47:43 +0300

    Feat: Refactored Socket list so I dont have to create a new reader and writer every time i need to access a socket.

---
+++ Big_Data_Management/src/com/data/SocketDetails.java
@@ -0,0 +1,72 @@
+package com.data;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketDetails {
+	private String hostname;
+	private Integer port;
+	private Socket socket;
+	private BufferedWriter writer;
+	private BufferedReader reader;
+	
+	public SocketDetails(String hostname, Integer port) throws UnknownHostException, IOException {
+		this.hostname = hostname;
+		this.port = port;
+		this.socket = new Socket(InetAddress.getByName(hostname), port);
+		this.writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+		this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+	
+	public boolean isClosed() {
+		if(socket == null) {
+			return true;
+		}
+		return socket.isClosed();
+	}
+	
+	public void close() throws IOException {
+		if(socket == null) {
+			return;
+		}
+		socket.close();
+	}
+	
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	public Socket getSocket() {
+		return socket;
+	}
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+	public BufferedWriter getWriter() {
+		return writer;
+	}
+	public void setWriter(BufferedWriter writer) {
+		this.writer = writer;
+	}
+	public BufferedReader getReader() {
+		return reader;
+	}
+	public void setReader(BufferedReader reader) {
+		this.reader = reader;
+	}
+	
+}
--- Big_Data_Management/src/com/main/KVBroker.java
+++ Big_Data_Management/src/com/main/KVBroker.java
@@ -5,9 +5,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -17,10 +14,12 @@ import java.util.Scanner;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.data.SocketDetails;
+
 
 public class KVBroker {
 	
-	private static List<Socket> sockets = new ArrayList<Socket>();
+	private static List<SocketDetails> sockets = new ArrayList<SocketDetails>();
 	private static List<String> data = new ArrayList<String>();
 	private static Random rand = new Random(System.currentTimeMillis());
 	private static String serverFilename;
@@ -49,24 +48,39 @@ public class KVBroker {
 		readData();
 		insertData();
 		
+		userCommands();
 		
 		closeConnections();
 	}
 	
-	private static boolean areArgumentsInvalid() {
-		if(serverFilename == null || dataFilename == null || replicationFactor == null ) {
-			return true;
+	private static void userCommands() {
+		Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			if(line.startsWith("GET")) {
+				// traverse all servers and return the data of the first one that answers smthing other than ERROR
+			} else if(line.startsWith("QUERY")) {
+				// traverse all servers and return the data of the first one that answers smthing other than ERROR
+			} else if(line.startsWith("DELETE")) {
+				// check if all servers are UP, if not print a message that the DELETE operation cannot be executed.
+			} else if(line.startsWith("PUT")) {
+				// might not be needed 
+			} else if(line.startsWith("EXIT")) {
+				System.out.println("Received exit command, initiating shutdown.");
+				break;
+			}
+			
 		}
-		return false;
+		scanner.close();
 	}
-
+	
 	private static void insertData() {
 		for(String dato : data) {
-			List<Socket> kSocks = getKSockets();
-			for (Socket sock : kSocks) {
+			List<SocketDetails> kSocks = getKSockets();
+			for (SocketDetails sock : kSocks) {
 				try {
-					BufferedWriter writer = new BufferedWriter(new PrintWriter(sock.getOutputStream()));
-					BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+					BufferedWriter writer = sock.getWriter();
+					BufferedReader reader = sock.getReader();
 					writer.append("PUT " + dato + System.lineSeparator());
 					writer.flush();
 					String response = reader.readLine();
@@ -81,7 +95,7 @@ public class KVBroker {
 		}
 	}
 	
-	private static List<Socket> getKSockets() {
+	private static List<SocketDetails> getKSockets() {
 		if(replicationFactor == sockets.size()) {
 			return sockets;
 		} else if(replicationFactor == sockets.size() - 1) {
@@ -90,14 +104,10 @@ public class KVBroker {
 			throw new RuntimeException("Replication factor is bigger than the available servers.");
 		}
 		// Else select randomly k servers from the socket list.
-		List<Socket> socks = new ArrayList<Socket>();
-		Socket lastSocket = null;
+		List<SocketDetails> socks = new ArrayList<SocketDetails>();
+		SocketDetails lastSocket = null;
 		while(socks.size() <= replicationFactor) {
-			Socket cur = getRandomSocket();
-			if(!pingSockets(cur)) { // If the selected socket is not reachable then skip it ( and remove it from the list (?) ) 
-				System.out.println("Selected socket (" + cur.getPort() + ") is not reachable.");
-				continue; 
-			}
+			SocketDetails cur = getRandomSocket();
 			if(cur != lastSocket) {
 				socks.add(cur);
 				lastSocket = cur;
@@ -106,7 +116,7 @@ public class KVBroker {
 		return socks;
 	}
 	
-	private static Socket getRandomSocket() {
+	private static SocketDetails getRandomSocket() {
 		if(sockets == null || sockets.isEmpty()) {
 			throw new RuntimeException("No socket found");
 		}
@@ -129,8 +139,7 @@ public class KVBroker {
 			String host = StringUtils.substringBefore(line, " ");
 			Integer port = Integer.parseInt(StringUtils.substringAfter(line, " "));
 			System.out.println("Socket: " + host + " " + port);
-			Socket socket = new Socket(InetAddress.getByName(host), port);
-			sockets.add(socket);
+			sockets.add(new SocketDetails(host, port));
 		}
 		scanner.close();
 	}
@@ -150,18 +159,25 @@ public class KVBroker {
 	}
 	
 	private static void closeConnections() {
-		for (Socket socket : sockets) {
+		for (SocketDetails socket : sockets) {
 			if(socket != null && !socket.isClosed()) {
 				try {
 					socket.close();
 				} catch (IOException e) {
-					System.out.println("Failed to close socket: " + socket.getInetAddress() + " " + socket.getPort());
+					System.out.println("Failed to close socket: " + socket.getHostname() + " " + socket.getPort());
 				}
 			}
 		}
 	}
 	
-	private static boolean pingSockets(Socket sock) {
+	private static boolean areArgumentsInvalid() {
+		if(serverFilename == null || dataFilename == null || replicationFactor == null ) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean pingSocket(Socket sock) {
 		try {
 			return sock.getInetAddress().isReachable(300);
 		} catch (IOException e) {
